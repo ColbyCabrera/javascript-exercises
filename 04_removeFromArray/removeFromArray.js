0da13626672af9ea182252e1184edb74408be80d
ColbyCabrera <gdthyispro@gmail.com>
2022-10-31 15:59:19 -0400

    pass remove from array tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...deletions) {
 
+    for (let i = 0; i < deletions.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (deletions[i] === arr[j]) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
