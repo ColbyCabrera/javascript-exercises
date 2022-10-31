e6561d119d3a222e18029332a34affccb7275d50
ColbyCabrera <gdthyispro@gmail.com>
2022-10-31 17:37:38 -0400

    pass sum all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    let start, end;
 
+    if (!(Number.isInteger(startNum) && Number.isInteger(endNum)) || (startNum < 0 || endNum < 0)) {
+        return "ERROR";
+    } else if (startNum < endNum) {
+        start = startNum;
+        end = endNum;
+    } else {
+        start = endNum;
+        end = startNum;
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
