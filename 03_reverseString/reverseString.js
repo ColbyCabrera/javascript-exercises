57ad546c90fb308ebe9b9971e26f00b5e00657ab
ColbyCabrera <gdthyispro@gmail.com>
2022-10-31 15:26:48 -0400

    pass reverse string tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    stringArray = [];
+    let reverseString = "";
+
+    for (let i = 0; i < string.length; i++) {
+        //stringArray.push(string.charAt(i));
+        reverseString = reverseString.concat(string.charAt((string.length - 1) - i));
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
