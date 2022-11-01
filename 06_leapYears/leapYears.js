47ce098955c3925fc71a746cffb9875f30848a0d
ColbyCabrera <gdthyispro@gmail.com>
2022-11-01 16:08:53 -0400

    Pass all leapYearas test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,28 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    
+    let isLeapYear;
+
+    if (year % 4 == 0) {
+
+        isLeapYear = true
+
+        if (year % 100 == 0) {
+
+            isLeapYear = false
+
+            if (year % 400 == 0) {
+                isLeapYear = true;
+            }
+        }
+    } else {
+        isLeapYear = false;
+    }
+
+    return isLeapYear;
+
+    
+
 
 };
 
